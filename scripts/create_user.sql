bc5d31a80413b3a9077cbb5b193d76e02c5673c3
savithri <savithri@codegen.net>
2021-01-09 16:29:48 +0530

    Refactor names

--- scripts/create_user.sql
+++ scripts/create_user.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS tblUser;
 
 CREATE TABLE tblUser 
 (   
-    userid char(36) NOT NULL default uuid(),
+    userId char(36) NOT NULL default uuid(),
     userName varchar(100) NOT NULL,
     userEmail varchar(100) NOT NULL,
-    Active BOOLEAN DEFAULT 1,
+    active BOOLEAN DEFAULT 1,
     PRIMARY KEY(userid)
 )
