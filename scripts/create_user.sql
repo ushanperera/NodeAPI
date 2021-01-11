ec5e9552ee64eb589f01b095f89858fb5a855985
savithri <savithri@codegen.net>
2021-01-11 19:15:55 +0530

    change attribute name

--- scripts/create_user.sql
+++ scripts/create_user.sql
@@ -6,7 +6,7 @@ CREATE TABLE tblUser
     userId char(36) NOT NULL default uuid(),
     firstName varchar(100) NOT NULL,
     lastName varchar(100) NOT NULL,
-    userEmail varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
     active BOOLEAN DEFAULT 1,
     admin BOOLEAN DEFAULT 1,
     PRIMARY KEY(userid)
