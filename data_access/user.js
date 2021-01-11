61d6c4e240e4aef1fa9e11428ef0d07ef1a567e0
savithri <savithri@codegen.net>
2021-01-11 23:33:44 +0530

    Adding user api

---
+++ data_access/user.js
@@ -0,0 +1,41 @@
+class User {
+    constructor(firstName, lastName, email, active, admin) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.active = active == true ? '1' : '0';
+        this.admin = admin == true ? '1' : '0';
+    }
+
+    static getAllUsers() {
+        let sql = `SELECT * FROM tblUser`;
+        return sql;
+    }
+
+    addUser() {
+        let sql = `INSERT INTO tblUser (firstName,lastName,email,active,admin)\
+                   VALUES('${this.firstName}','${this.lastName}','${this.email}','${this.active}','${this.admin}')`;
+        return sql;
+    }
+
+    static deleteUser(userId) {
+        let sql = `DELETE FROM tblUser WHERE userId = '${userId}'`;
+        return sql;
+    }
+
+    updateUser(userId) {
+        let sql = `UPDATE tblUser SET \
+        firstName = '${this.firstName}', \
+        lastName = '${this.lastName}', \
+        email = '${this.email}', \
+        active = '${this.active}', \
+        admin = '${this.admin}' \
+        WHERE userId = '${userId}'`;
+
+        return sql;
+    }
+
+
+}
+
+module.exports = User;
